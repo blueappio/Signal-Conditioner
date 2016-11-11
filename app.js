8f64f422873fa3c9ebcb3f2bb398ac5af0d6aa6b
mallikarjunaj <mallikarjuna.j@vensi.com>
2016-11-11 22:07:38 +0530

    Showing the error msg from the lib

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ app.controller('mainController', function ($scope, $mdToast, $mdDialog, bluCurre
             .catch(function (error) {
                 dismissLoadingIndicator();
                 console.error('Argh!', error, error.stack ? error.stack : '');
-                badToast('Unable to connect.');
+                badToast(error);
             });
     }
 
