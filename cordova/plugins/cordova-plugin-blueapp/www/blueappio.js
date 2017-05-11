b269cf395634e8c348aff3da891aee590c0c756f
mallikarjuna  j <mallikarjuna.j@vensi.com>
2017-05-11 17:46:06 +0530

    Updated the app

--- cordova/plugins/cordova-plugin-blueapp/www/blueappio.js
+++
@@ -1,7 +0,0 @@
-cordova.define("cordova-plugin-blueapp.blueappio", function(require, exports, module) {
-module.exports = {
-	request: function (action, message, successCallback, errorCallback) {
-		cordova.exec(successCallback, errorCallback, "IOBlueApp", action, [message]);
-	}
-};
-});
--- cordova/plugins/cordova-plugin-nordic-dfu/www/dfuimpl.js
+++
@@ -1,9 +0,0 @@
-cordova.define("cordova-plugin-nordic-dfu.dfuimpl", function(require, exports, module) {
-/*global cordova, module*/
-module.exports = {
-    request: function (action, message, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "DFUImpl", action, [message]);
-    }
-};
-
-});
